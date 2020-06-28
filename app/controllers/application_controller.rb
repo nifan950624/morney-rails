607466e60435d0fb4f30998a23c17231088a01cf
nifan950624 <nifan1950333426@qq.com>
2020-06-28 21:19:00 +0800

    优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: {user: resource}, status: 200
+    else
+      render json: {error: resource.errors}, status: 400
+    end
+  end
 end
